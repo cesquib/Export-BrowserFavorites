f09ae2e039696a6ccbba982cc7c412ed7e8e7a7e
yatbio <14935907+yatbio@users.noreply.github.com>
2018-03-26 15:50:00 -0400

    Init
    Sync

---
+++ sandbox_ie.ps1
@@ -0,0 +1,22 @@
+﻿function Get-DesktopShortcuts{
+    $Shortcuts = Get-ChildItem -Recurse "C:\users\mesquibel\favorites" -Include *.url
+    $Shell = New-Object -ComObject WScript.Shell
+    foreach ($Shortcut in $Shortcuts)
+    {
+        $Properties = @{
+        ShortcutName = $Shortcut.Name;
+        ShortcutFull = $Shortcut.FullName;
+        ShortcutPath = $shortcut.DirectoryName
+        Target = $Shell.CreateShortcut($Shortcut).targetpath
+        }
+        New-Object PSObject -Property $Properties
+    }
+
+[Runtime.InteropServices.Marshal]::ReleaseComObject($Shell) | Out-Null
+}
+
+$ieItems = Get-DesktopShortcuts
+ForEach ($ieItem in $ieItems) {
+    Write-Host $ieItem.ShortcutFull
+    }
+
---
+++ sandbox_sqlite.ps1
@@ -0,0 +1,28 @@
+﻿Add-Type -Path "C:\tools\scripts\lib\SQLLite\x64\System.Data.SQLite.dll"
+$con = New-Object -TypeName System.Data.SQLite.SQLiteConnection -ArgumentList "Read Only=True;"
+$con.ConnectionString = "Data Source=C:\Users\mesquibel\AppData\Roaming\Mozilla\Firefox\Profiles\13qbaq2q.default\places.sqlite"# CHANGE THIS 
+$con.Open()
+$sql = $con.CreateCommand()
+$sql.CommandText = "
+SELECT a.id AS ID, a.title AS Title, b.url AS URL, datetime(a.dateAdded/1000000,'unixepoch') AS DateAdded, datetime(a.LastModified/1000000,'unixepoch') AS LastModified, parents.title AS folder
+FROM moz_bookmarks AS a 
+JOIN moz_places AS b ON a.fk = b.id
+JOIN moz_bookmarks parents ON parents.id = a.parent"
+$adapter = New-Object -TypeName System.Data.SQLite.SQLiteDataAdapter $sql
+#we create the dataset
+$data = New-Object System.Data.DataSet
+#and then fill the dataset
+[void]$adapter.Fill($data)
+#we can, of course, then display the first one hundred rows in a grid
+ForEach ($entry in $data.tables[0].Rows) {
+<#
+    $entry.ID
+    $entry.Title
+    $entry.URL
+    $entry.DateAdded
+    $entry.LastModified
+    $entry.Folder
+#>
+}
+$con.Close()
+$con.Dispose()
